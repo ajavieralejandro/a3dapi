d64e5f8a8c0904a675d47a213388fe375b134b5f
ajavieralejandro <amorosijavier@gmail.com>
2020-09-26 15:01:54 -0300

    deploying to hostinger...

---
+++ database/migrations/2020_09_25_044246_create_docentes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docentes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('surname')->unique();
+            $table->string('email')->unique();
+            $table->string('profesion');
+            $table->longText('bio');
+            $table->longText('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docentes');
+    }
+}
